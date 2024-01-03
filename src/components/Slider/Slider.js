63583b75f097aee72f93c70b06c64f539806df05
shoxida <shoxidamoydinova0@gmail.com>
2024-01-03 12:08:14 +0500

    finished

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,30 +2,46 @@ import Slider from "react-slick";
 import slider1 from './img/slide-1.jpg'
 import slider2 from './img/slide-1.jpg'
 import slider3 from './img/slide-1.jpg'
+import style from './Slider.module.css'
 
 function SimpleSlider() {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+
         slidesToShow: 1,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 5000
+        autoplay: true,
+        speed: 1000,
+        //   autoplaySpeed: 1000,
+        cssEase: "linear"
     };
     return (
-        <Slider {...settings}>
-            <div>
-                <img style={{ width: '100%' }} src={slider1} alt="..." />
+        <div>
+            <div className={style.slider}>
+                <Slider {...settings}>
+                    <div>
+                        <h3 className={style.slayd}>
+                            {" "}
+                            <img src={slider1} alt="..." />
+                        </h3>
+                    </div>
+                    <div>
+                        <h3 className={style.slayd}>
+                            {" "}
+                            <img src={slider2} alt="..." />
+                        </h3>
+                    </div>
+                    <div>
+                        <h3 className={style.slayd}>
+                            {" "}
+                            <img src={slider3} alt="..." />
+                        </h3>
+                    </div>
+                </Slider>
             </div>
-            <div>
-                <img style={{ width: '100%' }} src={slider2} alt="..." />
-            </div>
-            <div>
-                <img style={{ width: '100%' }} src={slider3} alt="..." />
-            </div>
-        </Slider>
-    )
+        </div>
+    );
 }
 
 export default SimpleSlider
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,21 +2,25 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import style from './Navbar.module.css'
 import img from './img/navbarlogo.png'
+// import SimpleSlider from '../Slider/Slider'
 
 function Navbar() {
   return (
-    <div className={style.navbar}>
-      <Link to='/'><img src={img} alt="" /></Link>
-      <div className={style.navbarText}>
-        <div className={style.text1}>
-          <NavLink><h2>Yummy Pies</h2></NavLink>
-          <NavLink to='about'><h2>A Bit About Us</h2></NavLink>
-        </div>
-        <div className={style.text1}>
-          <NavLink to='provenanse'><h2>Provenance</h2></NavLink>
-          <NavLink to='GetTouch'><h2>Get In Touch</h2></NavLink>
+    <div className={style.Navbar}>
+      <div className={style.navbar}>
+        <Link to='/'><img src={img} alt="" /></Link>
+        <div className={style.navbarText}>
+          <div className={style.text1}>
+            <NavLink><h2>Yummy Pies</h2></NavLink>
+            <NavLink to='about'><h2>A Bit About Us</h2></NavLink>
+          </div>
+          <div className={style.text1}>
+            <NavLink to='provenanse'><h2>Provenance</h2></NavLink>
+            <NavLink to='GetTouch'><h2>Get In Touch</h2></NavLink>
+          </div>
         </div>
       </div>
+      {/* <SimpleSlider/> */}
     </div>
   )
 }
